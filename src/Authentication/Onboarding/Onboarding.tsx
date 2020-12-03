f878060995f1f91a993357d4dd9f493c5a69b077
Harun Kilic <harunkilic@live.dk>
2020-12-03 14:39:17 +0100

    Added slider

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,12 +1,123 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import Animated, { multiply } from "react-native-reanimated";
+import { interpolateColor, onScrollEvent, useValue } from "react-native-redash";
 
-interface Props {}
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Subslide from "./Subslide";
 
-const Onboarding = (props: Props) => {
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: BORDER_RADIUS,
+  },
+  footer: {
+    flex: 1,
+  },
+  footerContent: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+});
+
+const slides = [
+  {
+    title: "Relaxed",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfit? Don't worry! Find the best outfit here!",
+    color: "#BFEAF5",
+  },
+  {
+    title: "Playful",
+    subtitle: "Hear it First, Ware it First",
+    description:
+      "Heating the clothes in your wardrobe? Explore hundreds of outfit ideas",
+    color: "#BEECC4",
+  },
+  {
+    title: "Excentric",
+    subtitle: "Your Style, Your Way",
+    description:
+      "Create your individual & unique style and look amazing everyday",
+    color: "#FFE4D9",
+  },
+  {
+    title: "Funky",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the latest trends in fashion and explore your personality",
+    color: "#FFDDDD",
+  },
+];
+
+const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
+
   return (
-    <View style={{ flex: 1, backgroundColor: "cyan" }}>
-      <Text>OnBoarding</Text>
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ title }, index) => (
+            <Slide {...{ title }} right={!!(index % 2)} key={index} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <Animated.View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <Animated.View
+          style={[
+            styles.footerContent,
+            {
+              width: width * slides.length,
+              flex: 1,
+              transform: [{ translateX: multiply(x, -1) }],
+            },
+          ]}
+        >
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              onPress={() => {
+                if (scroll.current) {
+                  scroll.current
+                    .getNode()
+                    .scrollTo({ x: width * (index + 1), animated: true });
+                }
+              }}
+              last={index === slides.length - 1}
+              {...{ subtitle, description }}
+            />
+          ))}
+        </Animated.View>
+      </Animated.View>
     </View>
   );
 };
