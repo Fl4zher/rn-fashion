038c6708f324b8c29e5e6f250930db301ed9e255
Harun Kilic <harunkilic@live.dk>
2020-12-07 10:22:02 +0100

    Added formik to login

--- src/Authentication/components/Form/Checkbox.tsx
+++ src/Authentication/components/Form/Checkbox.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
-import { Box, Text } from '../../../components';
-import { Feather as Icon } from '@expo/vector-icons'
-import { RectButton } from 'react-native-gesture-handler';
+import React, { useState } from "react";
+import { Feather as Icon } from "@expo/vector-icons";
+import { RectButton } from "react-native-gesture-handler";
+
+import { Box, Text } from "../../../components";
 
 interface Props {
-    label: string
+  label: string;
+  checked: boolean;
+  onChange: () => void;
 }
 
-const Checkbox = ({ label }: Props) => {
-    const [checked, setChecked] = useState(false);
-    return (
-        <RectButton onPress={() => setChecked(c => !c)} style={{ justifyContent: 'center' }}>
-            <Box flexDirection="row">
-                <Box marginRight="m" height={20} width={20} alignItems="center" justifyContent="center" borderWidth={1} borderColor="primary" borderRadius="s" backgroundColor={checked ? "primary" : "white"}>
-                    <Icon name="check" color="white" />
-                </Box>
-                <Text variant="button">{label}</Text>
-            </Box>
-        </RectButton>
-    )
-}
+const Checkbox = ({ label, onChange, checked }: Props) => {
+  return (
+    <RectButton onPress={() => onChange()} style={{ justifyContent: "center" }}>
+      <Box flexDirection="row">
+        <Box
+          marginRight="m"
+          height={20}
+          width={20}
+          alignItems="center"
+          justifyContent="center"
+          borderWidth={1}
+          borderColor="primary"
+          borderRadius="s"
+          backgroundColor={checked ? "primary" : "white"}
+        >
+          <Icon name="check" color="white" />
+        </Box>
+        <Text variant="button">{label}</Text>
+      </Box>
+    </RectButton>
+  );
+};
 
-export default Checkbox
+export default Checkbox;
--- src/Authentication/components/Form/TextInput.tsx
+++ src/Authentication/components/Form/TextInput.tsx
@@ -1,36 +1,56 @@
-import React, { useState } from 'react'
-import { StyleSheet, TextInput as Input, TextInputProps } from 'react-native'
-import { Box, theme } from '../../../components'
-import { Feather as Icon } from '@expo/vector-icons'
+import React from "react";
+import { StyleSheet, TextInput as Input, TextInputProps } from "react-native";
+import { Feather as Icon } from "@expo/vector-icons";
+
+import { Box, theme } from "../../../components";
 
 interface Props extends TextInputProps {
-    placeholder: string;
-    icon: string;
+  placeholder: string;
+  icon: string;
+  error?: string;
+  touched?: boolean;
 }
 
 const SIZE = theme.borderRadii.m * 2;
 
-const TextInput = ({ icon, ...props }: Props) => {
-    const reColor = "danger";
-    const color = theme.colors[reColor];
-    return (
-        <Box padding="s" flexDirection="row" height={48} borderRadius="s" borderColor={reColor} borderWidth={StyleSheet.hairlineWidth} alignItems="center">
-            <Box padding="s">
-                <Icon name={icon as any} size={16} {...{ color }} />
-            </Box>
-            <Box flex={1}>
-
-                <Input underlineColorAndroid="transparent" placeholderTextColor={color} {...props} />
-            </Box>
-            {
-                (false) && (
-                    <Box height={SIZE} width={SIZE} borderRadius="m" backgroundColor={reColor} alignItems="center" justifyContent="center" >
-                        <Icon name={true ? "check" : "x"} color="white" size={16} />
-                    </Box>
-                )
-            }
+const TextInput = ({ icon, error, touched, ...props }: Props) => {
+  // eslint-disable-next-line no-nested-ternary
+  const reColor = !touched ? "darkGrey" : error ? "danger" : "primary";
+  const color = theme.colors[reColor];
+  return (
+    <Box
+      padding="s"
+      flexDirection="row"
+      height={48}
+      borderRadius="s"
+      borderColor={reColor}
+      borderWidth={StyleSheet.hairlineWidth}
+      alignItems="center"
+    >
+      <Box padding="s">
+        <Icon name={icon as never} size={16} {...{ color }} />
+      </Box>
+      <Box flex={1}>
+        <Input
+          underlineColorAndroid="transparent"
+          placeholderTextColor={color}
+          {...props}
+        />
+      </Box>
+      {touched && (
+        <Box
+          height={SIZE}
+          width={SIZE}
+          borderRadius="m"
+          backgroundColor={reColor}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Icon name={!error ? "check" : "x"} color="white" size={16} />
         </Box>
-    )
-}
+      )}
+    </Box>
+  );
+};
 
-export default TextInput
+export default TextInput;
